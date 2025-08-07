ab94b59f5295ab4e957219105625a0d9b92f1a88
Brian Magaha <btmy87@gmail.com>
2025-08-07 18:03:14 -0400

    Adding help to examples bat file

--- get_example_files.bat
+++ get_example_files.bat
@@ -1,4 +1,8 @@
 :: script to download example SDF files from NIST
+:: These are just a couple of interesting examples
+:: you can download many others from webbook.nist.gov
+:: search for the chemical you're interested in
+:: click "3d SD file" underneath the Chemical structure header to download
 
 :: caffeine
 curl https://webbook.nist.gov/cgi/cbook.cgi?Str3File=C58082 -o caffeine.sdf
