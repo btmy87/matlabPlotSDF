7a61f31e638b13935a3e2317ed81116ee70ba10f
Brian Magaha <btmy87@gmail.com>
2025-08-07 17:59:09 -0400

    Adding script to download example files

---
+++ get_example_files.bat
@@ -0,0 +1,13 @@
+:: script to download example SDF files from NIST
+
+:: caffeine
+curl https://webbook.nist.gov/cgi/cbook.cgi?Str3File=C58082 -o caffeine.sdf
+
+:: tnt
+curl https://webbook.nist.gov/cgi/cbook.cgi?Str3File=C118967 -o tnt.sdf
+
+:: benzene
+curl https://webbook.nist.gov/cgi/cbook.cgi?Str3File=C71432 -o benzene.sdf
+
+:: buckminsterfullerene
+curl https://webbook.nist.gov/cgi/cbook.cgi?Str3File=C99685968 -o C60.sdf
